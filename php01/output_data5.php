86ea1b2abc871b905756db300856933253d66b76
GRAMxJUNK <Hiroki@gjpc.local>
2017-06-12 23:46:26 +0900

    201706122346提出するんじゃ！

--- php01/output_data5.php
+++ php01/output_data5.php
@@ -4,6 +4,13 @@ $reformDataCsv = 'data/reform_data.csv';//　リフォームのデータベー�
 $constNewDataCsv = 'data/constNew_data.csv';//　新築のデータベース
 
 if(isset($_GET["id"]) && $_GET["id"]=="reform"){
+	if($_GET["name"]==="JSON"||$_GET["name"]==="Jason"||$_GET["name"]==="ジェイソン"||$_GET["name"]==="json"||$_GET["name"]==="jason"){
+	$name3=$_GET['name'];
+	$strJason="{$name3}";
+	$json3 = json_encode($strJason);
+	echo $json3;                 //jsonを表示
+	exit;
+	}
 
 	$name=$_GET['name'];
 	$yourAge=$_GET['yourAge'];
@@ -49,6 +56,50 @@ if(isset($_GET["id"]) && $_GET["id"]=="reformGet"){
 }
 
 
+
+if(isset($_GET["id"]) && $_GET["id"]=="constNew"){
+
+	$name2=$_GET['name2'];
+	$yourAge2=$_GET['yourAge2'];
+	$why2=$_GET['why2'];
+	$decide2=$_GET['decide2'];
+	$satisfaction2=$_GET['satisfaction2'];
+	$reason2=$_GET['reason2'];
+
+	//$str=$name.",".$mail.",".$age."\n";
+	$str2="{$name2},{$yourAge2},{$why2},{$decide2},{$satisfaction2},{$reason2}\n";
+
+	$createCsv2 = fopen($constNewDataCsv,"a");// ファイル読み込み　,"a"で、なければ新規作成という命令;
+	flock($createCsv2, LOCK_EX);// ファイルロック
+	fwrite($createCsv2, $str2);
+	flock($createCsv2, LOCK_UN);// ファイルロック解除
+	fclose($createCsv2);
+
+	$strExp2=explode(",",$str2);
+	$json2 = json_encode($strExp2);
+	echo $json2;                 //jsonを表示
+	exit;
+}
+
+if(isset($_GET["id"]) && $_GET["id"]=="constNewGet"){
+	$cNData = array();
+	$openConstNewData = fopen($constNewDataCsv,"r");
+	while (!feof($openConstNewData)) {
+		$cNDataRecord = fgets($openConstNewData);
+		$cNDataRecord2=str_replace( "\r\n", "", $cNDataRecord);
+		$cNDataRecord3=str_replace( "\n", "", $cNDataRecord2);
+		$cNDataRecord4=str_replace( "\r", "", $cNDataRecord3);
+		$cNDR=explode(",",$cNDataRecord4);
+		for($i=0;$i<count($cNDR);$i++){
+			$cNDR[$i] = xss($cNDR[$i]);
+		}
+		array_push($cNData, $cNDR);
+	}
+	fclose($openConstNewData);
+	$json2 = json_encode($cNData);
+	echo $json2;
+	exit;
+}
 
 
 
--- php01/script.js
+++ php01/script.js
@@ -1,18 +1,26 @@
 $("#reformArea").hide();
+$("#constNewArea").hide();
 $("#reformForm").hide();
 $("#constNewForm").hide();
+$("#thanks").hide();
 
 $("#reformBtn").on("click", function() {
+	$("#constNewForm").hide();
 	$("#reformForm").fadeIn(500);
-	$("#reformBtn").hide();
-	$("#constNewBtn").hide();
 });
 
 $("#constNewBtn").on("click", function() {
+	$("#reformForm").hide();
 	$("#constNewForm").fadeIn(500);
-	$("#constNewBtn").hide();
-	$("#reformBtn").hide();
 });
+
+/*＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
+＊　リフォームのアンケート
+
+＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊*/
+
+
 
 $("#reformSend").on("click", function(e) {
 	e.preventDefault();
@@ -20,14 +28,25 @@ $("#reformSend").on("click", function(e) {
 	//JSON取得
 	$.getJSON("output_data5.php?id=reform", userForm, function(reformUser) {
 		console.log(reformUser);
+		if(reformUser==="JSON"||reformUser==="Jason"||reformUser==="ジェイソン"||reformUser==="json"||reformUser==="jason"){
+			let json="thanks! ";
+			json+='<img src="img/f-vs-jason.jpg">';
+			$("#thanks").empty();
+			$("#thanks").append(json);
+			$("#thanks").fadeIn(1000);
+			return;
+		}
 		let span = "<span>thaks! ";
 		span += reformUser[0];
 		span += "</span>";
-		$("#thnks").append(span);
+		$("#thanks").empty();
+		$("#thanks").append(span);
+		$("#thanks").fadeIn(1000);
 	});
 });
 
 $("#reformGet").on("click", function() {
+	$("#constNewArea").hide();
 	$("#reformArea").fadeIn(1500);
 	$.getJSON("output_data5.php?id=reformGet", function(reformData) {
 		console.log(reformData);
@@ -129,7 +148,7 @@ $("#reformGet").on("click", function() {
 			if (reformData[val][3] === "mySpace")why7.push(reformData[val][3]);
 			if (reformData[val][3] === "cleanUp")why8.push(reformData[val][3]);
 			if (reformData[val][3] === "oldAge")why9.push(reformData[val][3]);
-			if (reformData[val][3] === "Care")why10.push(reformData[val][3]);
+			if (reformData[val][3] === "care")why10.push(reformData[val][3]);
 			if (reformData[val][3] === "earthquake")why11.push(reformData[val][3]);
 			if (reformData[val][3] === "environment")why12.push(reformData[val][3]);
 		}
@@ -238,6 +257,215 @@ $("#reformGet").on("click", function() {
 				reasonList+=reformData[val][6];
 				reasonList+=`</li>`;
 				$("#reasonList").append(reasonList);
+			}
+		}
+	});//　getjson
+});//　clickfunction
+
+
+/*＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
+＊　新築のアンケート
+
+＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊*/
+
+
+$("#constNewSend").on("click", function(e) {
+	e.preventDefault();
+	let userForm2 = $("#constNewForm").serializeArray();
+	//JSON取得
+	$.getJSON("output_data5.php?id=constNew", userForm2, function(constNewUser) {
+		console.log(constNewUser);
+		let span = "<span>thaks! ";
+		span += constNewUser[0];
+		span += "</span>";
+		$("#thanks").empty();
+		$("#thanks").append(span);
+		$("#thanks").fadeIn(1000);
+	});
+});
+
+$("#constNewGet").on("click", function() {
+	$("#reformArea").hide();
+	$("#constNewArea").fadeIn(1500);
+	$.getJSON("output_data5.php?id=constNewGet", function(constNewData) {
+		console.log(constNewData);
+		//　DATA表示　数ループ処理
+		//　質問１
+		$("#yourAgeDataGraph2").empty();
+		let yourAge2_0 = [];
+		let yourAge2_1 = [];
+		let yourAge2_2 = [];
+		let yourAge2_3 = [];
+		for (val in constNewData) {
+			if (constNewData[val][1] === "0")yourAge2_0.push(constNewData[val][1]);
+			if (constNewData[val][1] === "1")yourAge2_1.push(constNewData[val][1]);
+			if (constNewData[val][1] === "2")yourAge2_2.push(constNewData[val][1]);
+			if (constNewData[val][1] === "3")yourAge2_3.push(constNewData[val][1]);
+		}
+		const ctxYourAgeDataGraph2 = document.getElementById("yourAgeDataGraph2").getContext("2d");
+		const yourAgeDataGraph2 = new Chart(ctxYourAgeDataGraph2, {
+			type: 'doughnut',
+			data: {
+				labels: ["〜３０歳", "３１〜５０歳", "５１〜７０歳", "７１歳〜"],
+				datasets: [{
+					backgroundColor: [
+						"#f00",
+						"#0f0",
+						"#00f",
+						"#f0f"
+					],
+					data: [yourAge2_0.length,yourAge2_1.length,yourAge2_2.length,yourAge2_3.length]
+				}]
+			},
+			options:{
+				title: {
+					display: true,
+					text: '回答者の年齢層は？'
+				}
+			}
+		});
+		//　質問２
+		$("#whyDataGraph2").empty();
+		let why2_0 = [];
+		let why2_1 = [];
+		let why2_2 = [];
+		let why2_3 = [];
+		let why2_4 = [];
+		let why2_5 = [];
+		let why2_6 = [];
+		let why2_7 = [];
+		let why2_8 = [];
+		let why2_9 = [];
+		let why2_10 = [];
+		let why2_11 = [];
+		let why2_12 = [];
+		for (val in constNewData) {
+			if (constNewData[val][2] === "trouble")why2_0.push(constNewData[val][2]);
+			if (constNewData[val][2] === "renewal")why2_1.push(constNewData[val][2]);
+			if (constNewData[val][2] === "child")why2_2.push(constNewData[val][2]);
+			if (constNewData[val][2] === "grandChild")why2_3.push(constNewData[val][2]);
+			if (constNewData[val][2] === "residentsDecreased")why2_4.push(constNewData[val][2]);
+			if (constNewData[val][2] === "pet")why2_5.push(constNewData[val][2]);
+			if (constNewData[val][2] === "dream")why2_6.push(constNewData[val][2]);
+			if (constNewData[val][2] === "mySpace")why2_7.push(constNewData[val][2]);
+			if (constNewData[val][2] === "cleanUp")why2_8.push(constNewData[val][2]);
+			if (constNewData[val][2] === "oldAge")why2_9.push(constNewData[val][2]);
+			if (constNewData[val][2] === "care")why2_10.push(constNewData[val][2]);
+			if (constNewData[val][2] === "earthquake")why2_11.push(constNewData[val][2]);
+			if (constNewData[val][2] === "environment")why2_12.push(constNewData[val][2]);
+		}
+		const ctxWhyDataGraph2 = document.getElementById("whyDataGraph2").getContext("2d");
+		const whyDataGraph2 = new Chart(ctxWhyDataGraph2, {
+			type: 'doughnut',
+			data: {
+				labels: ["旧宅の雨漏りや故障による建て替え","旧宅の老朽化による建て替え ","子どもが生まれた","孫が生まれた","住人が減った","ペットのため","一軒家に住みたかった","自分の居場所が欲しい","片付いた家にしたい","老後のため","介護のため","地震のため","環境のため"],
+				datasets: [{
+					backgroundColor: [
+						"#f00",
+						"#0f0",
+						"#00f",
+						"#f0f",
+						"#ff0",
+						"#0ff",
+						"#900",
+						"#090",
+						"#009",
+						"#909",
+						"#990",
+						"#099",
+						"#300"
+					],
+					data: [why2_0.length,why2_1.length,why2_2.length,why2_3.length,why2_4.length,why2_5.length,why2_6.length,why2_7.length,why2_8.length,why2_9.length,why2_10.length,why2_11.length,why2_12.length]
+				}]
+			},
+			options:{
+				title: {
+					display: true,
+					text: '一軒家購入（検討）のきっかけは？'
+				}
+			}
+		});
+		//　質問３
+		$("#decideDataGraph2").empty();
+		let decide2_0 = [];
+		let decide2_1 = [];
+		let decide2_2 = [];
+		let decide2_3 = [];
+		let decide2_4 = [];
+		let decide2_5 = [];
+		for (val in constNewData) {
+			if (constNewData[val][3] === "price")decide2_0.push(constNewData[val][3]);
+			if (constNewData[val][3] === "function")decide2_1.push(constNewData[val][3]);
+			if (constNewData[val][3] === "durable")decide2_2.push(constNewData[val][3]);
+			if (constNewData[val][3] === "proposal")decide2_3.push(constNewData[val][3]);
+			if (constNewData[val][3] === "design")decide2_4.push(constNewData[val][3]);
+			if (constNewData[val][3] === "location")decide2_5.push(constNewData[val][3]);
+		}
+		const ctxDecideDataGraph2 = document.getElementById("decideDataGraph2").getContext("2d");
+		const decideDataGraph2 = new Chart(ctxDecideDataGraph2, {
+			type: 'doughnut',
+			data: {
+				labels: ["値段", "機能性", "耐久性", "提案力", "デザイン・見た目", "立地"],
+				datasets: [{
+					backgroundColor: [
+						"#f00",
+						"#0f0",
+						"#00f",
+						"#f0f",
+						"#ff0",
+						"#0ff"
+					],
+					data: [decide2_0.length,decide2_1.length,decide2_2.length,decide2_3.length,decide2_4.length,decide2_5.length]
+				}]
+			},
+			options:{
+				title: {
+					display: true,
+					text: '一軒家購入（検討）の決め手は？'
+				}
+			}
+		});
+		//　質問４
+		$("#satisfactionDataGraph2").empty();
+		let satisfaction2_0 = [];
+		let satisfaction2_1 = [];
+		let satisfaction2_2 = [];
+		for (val in constNewData) {
+			if (constNewData[val][4] === "0")satisfaction2_0.push(constNewData[val][4]);
+			if (constNewData[val][4] === "1")satisfaction2_1.push(constNewData[val][4]);
+			if (constNewData[val][4] === "2")satisfaction2_2.push(constNewData[val][4]);
+		}
+		const ctxSatisfactionDataGraph2 = document.getElementById("satisfactionDataGraph2").getContext("2d");
+		const satisfactionDataGraph2 = new Chart(ctxSatisfactionDataGraph2, {
+			type: 'doughnut',
+			data: {
+				labels: ["満足", "普通", "不満"],
+				datasets: [{
+					backgroundColor: [
+						"#f00",
+						"#0f0",
+						"#00f"
+					],
+					data: [satisfaction2_0.length,satisfaction2_1.length,satisfaction2_2.length]
+				}]
+			},
+			options:{
+				title: {
+					display: true,
+					text: '一軒家購入の満足度（期待値）は？'
+				}
+			}
+		});
+		//　質問５
+		$("#reasonList2").empty();
+		let reasonList2;
+		for (val in constNewData) {
+			if(constNewData[val][5]!==undefined){
+				reasonList2= `<li>`;
+				reasonList2+=constNewData[val][5];
+				reasonList2+=`</li>`;
+				$("#reasonList2").append(reasonList2);
 			}
 		}
 	});//　getjson
