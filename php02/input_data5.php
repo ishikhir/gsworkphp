503e5956c6f477affc3b87a6f5bf095156c0db29
石川家 <Ishikawa_Family@ishikhirMBA.local>
2017-06-15 10:19:57 +0900

    課題を提出じゃ201606151019

--- php02/input_data5.php
+++ php02/input_data5.php
@@ -1,5 +1,150 @@
 <?php
 include("functions.php");
+
+
+//if(isset($_GET["id"]) && $_GET["id"]=="reformGet"){
+	try {
+		$pdo = new PDO('mysql:dbname=myHome;charset=utf8;host=localhost','root','');
+	}catch (PDOException $e) {
+		exit('DbConnectError:'.$e->getMessage());
+	}
+
+	//　年齢の処理
+
+	$yourAge = $pdo->prepare(
+		"SELECT
+			CASE
+				WHEN yourAge BETWEEN 0 AND 29 THEN 'rya029'
+				WHEN yourAge BETWEEN 30 AND 49 THEN 'rya3049'
+				WHEN yourAge BETWEEN 50 and 69 THEN 'rya5069'
+				WHEN yourAge >= 70 THEN 'rya70'
+			END
+		AS agegroup, count(yourAge) AS total
+		FROM reform_q
+		GROUP BY agegroup");
+	$ya = $yourAge->execute();
+	//３．データ表示
+	$viewya="";
+	if($ya==false){
+		$errya = $yourAge->errorInfo();
+		exit("ErrorQuery:".$errya[2]);
+	}else{
+		while( $resya = $yourAge->fetch(PDO::FETCH_ASSOC)){
+			$viewya .='<p class="yourAge" id="'.$resya["agegroup"].'">'.$resya["total"].'</p>';
+		}
+	}
+
+	//　築年数の処理
+
+	$homeAge = $pdo->prepare(
+		"SELECT
+			CASE
+				WHEN homeAge BETWEEN 0 AND 9 THEN 'rha09'
+				WHEN homeAge BETWEEN 10 AND 19 THEN 'rha1019'
+				WHEN homeAge BETWEEN 20 and 29 THEN 'rha2029'
+				WHEN homeAge BETWEEN 30 and 39 THEN 'rha3039'
+				WHEN homeAge >= 40 THEN 'rha40'
+			END
+		AS agegroup, count(homeAge) AS total,count(homeAge)*100/(SELECT count(homeAge) FROM reform_q) AS percent
+		FROM reform_q
+		GROUP BY agegroup");
+	$ha = $homeAge->execute();
+	$viewha="";
+	if($ha==false){
+		$errha = $homeAge->errorInfo();
+		exit("ErrorQuery:".$errha[2]);
+	}else{
+		while( $resha = $homeAge->fetch(PDO::FETCH_ASSOC)){
+			$viewha .='<p class="homeAge" id="'.$resha["agegroup"].'">'.$resha["percent"].'</p>';
+		}
+	}
+
+
+//　きっかけの処理
+
+	$why = $pdo->prepare("SELECT COUNT(*) AS record,why FROM reform_q GROUP BY why ORDER BY record DESC");
+	$whyex = $why->execute();
+	$viewwhy="";
+	if($whyex==false){
+		$errwhy = $why->errorInfo();
+		exit("ErrorQuery:".$errwhy[2]);
+	}else{
+		while( $reswhy = $why->fetch(PDO::FETCH_ASSOC)){
+			$viewwhy .='<p class="why" id="'.$reswhy["why"].'">'.$reswhy["record"].'</p>';
+		}
+	}
+
+
+//　決め手の処理
+
+	$decide = $pdo->prepare("SELECT COUNT(*) AS record,decide FROM reform_q GROUP BY decide ORDER BY record DESC");
+	$decideex = $decide->execute();
+	$viewdec="";
+	if($decideex==false){
+		$errdec = $decide->errorInfo();
+		exit("ErrorQuery:".$errdec[2]);
+	}else{
+		while( $resdec = $decide->fetch(PDO::FETCH_ASSOC)){
+			$viewdec .='<p class="decide" id="'.$resdec["decide"].'">'.$resdec["record"].'</p>';
+		}
+	}
+
+
+//　満足度の処理
+
+	$satisfaction = $pdo->prepare("SELECT COUNT(*) AS record,satisfaction FROM reform_q GROUP BY satisfaction ORDER BY record DESC");
+	$satisfactionex = $satisfaction->execute();
+	$viewsat="";
+	if($satisfactionex==false){
+		$errsat = $satisfaction->errorInfo();
+		exit("ErrorQuery:".$errsat[2]);
+	}else{
+		while( $ressat = $satisfaction->fetch(PDO::FETCH_ASSOC)){
+			$viewsat .='<p class="satisfaction" id="'.$ressat["satisfaction"].'">'.$ressat["record"].'</p>';
+		}
+	}
+
+
+//　コメントの処理
+
+	$reason = $pdo->prepare("SELECT id,reason FROM reform_q");
+	$reasonex = $reason->execute();
+	$viewrea="";
+	if($reasonex==false){
+		$errrea = $reason->errorInfo();
+		exit("ErrorQuery:".$errrea[2]);
+	}else{
+		while( $resrea = $reason->fetch(PDO::FETCH_ASSOC)){
+			$viewrea .='<li class="reason" id="reason'.$resrea["id"].'">'.$resrea["reason"].'</li>';
+		}
+	}
+
+
+
+
+if(isset($_GET["id"]) && $_GET["id"]=="constNewGet"){
+	$cNData = array();
+	$openConstNewData = fopen($constNewDataCsv,"r");
+	while (!feof($openConstNewData)) {
+		$cNDataRecord = fgets($openConstNewData);
+		$cNDataRecord2=str_replace( "\r\n", "", $cNDataRecord);
+		$cNDataRecord3=str_replace( "\n", "", $cNDataRecord2);
+		$cNDataRecord4=str_replace( "\r", "", $cNDataRecord3);
+		$cNDR=explode(",",$cNDataRecord4);
+		for($i=0;$i<count($cNDR);$i++){
+			$cNDR[$i] = xss($cNDR[$i]);
+		}
+		array_push($cNData, $cNDR);
+	}
+	fclose($openConstNewData);
+	$json2 = json_encode($cNData);
+	echo $json2;
+	exit;
+}
+
+
+
+
 ?>
 
 <html>
@@ -103,6 +248,16 @@ include("functions.php");
 		<button id="reformGet">リフォームアンケートの結果確認</button>
 		<button id="constNewGet">新築アンケートの結果確認</button>
 	</div>
+	<div id="output1">
+		<?=$viewya ?>
+		<?=$viewha ?>
+		<?=$viewwhy ?>
+		<?=$viewdec ?>
+		<?=$viewsat ?>
+		<ul id="rReason">
+			<?=$viewrea ?>
+		</ul>
+	</div>
 	<div id="reformArea">
 		<div class="graph"><canvas id="yourAgeDataGraph" width="300" height="200"></canvas></div>
 		<div class="graph"><canvas id="homeAgeDataGraph" width="300" height="200"></canvas></div>
@@ -110,7 +265,7 @@ include("functions.php");
 		<div class="graph"><canvas id="decideDataGraph" width="300" height="200"></canvas></div>
 		<div class="graph"><canvas id="satisfactionDataGraph" width="300" height="200"></canvas></div>
 		<div><strong>その満足度の理由は？</strong></div>
-		<ul id="reasonList"></ul>
+		<ul id="reasonList"><?=$viewrea ?></ul>
 	</div>
 	<div id="constNewArea">
 		<div class="graph"><canvas id="yourAgeDataGraph2" width="300" height="200"></canvas></div>
--- php02/output_data5.php
+++ php02/output_data5.php
@@ -51,50 +51,6 @@ if(isset($_GET["id"]) && $_GET["id"]=="reform"){
 }
 
 
-if(isset($_GET["id"]) && $_GET["id"]=="reformGet"){
-	$rData = array();
-
-//	try {
-//		$pdo2 = new PDO('mysql:dbname=myHome;charset=utf8;host=localhost','root','');
-//	}catch (PDOException $e) {
-//		exit('DbConnectError:'.$e->getMessage());
-//	}
-//
-//	$stmt2 = $pdo2->prepare("SELECT * FROM reform_q ORDER BY id DESC");
-//	$status2 = $stmt2->execute();
-//
-//	//３．データ表示
-//	$view2="";
-//	if($status2==false){
-//		$error2 = $stmt2->errorInfo();
-//		exit("ErrorQuery:".$error2[2]);
-//	}else{
-//		while( $result2 = $stmt2->fetch(PDO::FETCH_ASSOC)){
-//			$view .= "<p>".$result["name"].",".$result["email"]."</p>";
-//	  }
-//
-//	}
-
-	$openReformData = fopen($reformDataCsv,"r");
-	while (!feof($openReformData)) {
-		$rDataRecord = fgets($openReformData);
-		$rDataRecord2=str_replace( "\r\n", "", $rDataRecord);
-		$rDataRecord3=str_replace( "\n", "", $rDataRecord2);
-		$rDataRecord4=str_replace( "\r", "", $rDataRecord3);
-		$rDR=explode(",",$rDataRecord4);
-		for($i=0;$i<count($rDR);$i++){
-			$rDR[$i] = xss($rDR[$i]);
-		}
-		array_push($rData, $rDR);
-	}
-//	fclose($openReformData);
-	$json2 = json_encode($rData);
-	echo $json2;
-	exit;
-}
-
-
-
 if(isset($_GET["id"]) && $_GET["id"]=="constNew"){
 
 	$name2=$_GET['name2'];
@@ -131,42 +87,6 @@ if(isset($_GET["id"]) && $_GET["id"]=="constNew"){
 		echo $json3;                 //jsonを表示
 		exit;
 	}
-}
-
-
-	//$str=$name.",".$mail.",".$age."\n";
-//	$str2="{$name2},{$yourAge2},{$why2},{$decide2},{$satisfaction2},{$reason2}\n";
-//
-//	$createCsv2 = fopen($constNewDataCsv,"a");// ファイル読み込み　,"a"で、なければ新規作成という命令;
-//	flock($createCsv2, LOCK_EX);// ファイルロック
-//	fwrite($createCsv2, $str2);
-//	flock($createCsv2, LOCK_UN);// ファイルロック解除
-//	fclose($createCsv2);
-//
-//	$strExp2=explode(",",$str2);
-//	$json2 = json_encode($strExp2);
-//	echo $json2;                 //jsonを表示
-//	exit;
-//}
-
-if(isset($_GET["id"]) && $_GET["id"]=="constNewGet"){
-	$cNData = array();
-	$openConstNewData = fopen($constNewDataCsv,"r");
-	while (!feof($openConstNewData)) {
-		$cNDataRecord = fgets($openConstNewData);
-		$cNDataRecord2=str_replace( "\r\n", "", $cNDataRecord);
-		$cNDataRecord3=str_replace( "\n", "", $cNDataRecord2);
-		$cNDataRecord4=str_replace( "\r", "", $cNDataRecord3);
-		$cNDR=explode(",",$cNDataRecord4);
-		for($i=0;$i<count($cNDR);$i++){
-			$cNDR[$i] = xss($cNDR[$i]);
-		}
-		array_push($cNData, $cNDR);
-	}
-	fclose($openConstNewData);
-	$json2 = json_encode($cNData);
-	echo $json2;
-	exit;
 }
 
 
